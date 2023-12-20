f6798e282e80f80d6db8619d28112ff6f2b7f52d
ashwinidas97 <ashwinidas1997@gmail.com>
2023-12-20 15:23:43 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:amazon_clone/layout/screen_layout.dart';
+import 'package:amazon_clone/providers/user_details_provider.dart';
+//import 'package:amazon_clone/screens/sell_screen.dart';
+//import 'package:amazon_clone/model/product_model.dart';
+//import 'package:amazon_clone/screens/product_screen.dart';
+//import 'package:amazon_clone/screens/results_screen.dart';
+import 'package:amazon_clone/screens/sign_in_screen.dart';
+import 'package:amazon_clone/utils/color_themes.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+   await Firebase.initializeApp(
+      options: const FirebaseOptions(
+  apiKey: "AIzaSyA_C_sZ7wM2XCc2H5w0y0blvzw-YrerL7U",
+  authDomain: "clone-3c7aa.firebaseapp.com",
+  projectId: "clone-3c7aa",
+  storageBucket: "clone-3c7aa.appspot.com",
+  messagingSenderId: "777436934594",
+  appId: "1:777436934594:web:c7de61d594a4ce9f54265b",
+  ));
+  }else{
+   await Firebase.initializeApp();
+  }
+  runApp(const AmazonClone());
+}
+class AmazonClone extends StatelessWidget {
+  const AmazonClone({Key? key }) : super(key: key);
+
+  @override
+ 
+  Widget build(BuildContext context) {
+
+    return MultiProvider(
+      providers: [ChangeNotifierProvider(create: (_)=> UserDetailsProvider())],
+      child: MaterialApp(
+        title: "Amazon Clone",
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.light().copyWith(
+          scaffoldBackgroundColor: backgroundColor,
+        ),
+        home: StreamBuilder(stream: FirebaseAuth.instance.authStateChanges(), 
+        builder: (context, AsyncSnapshot<User?> user){
+           if (user.connectionState == ConnectionState.waiting){
+              return const Center(
+                child: CircularProgressIndicator(
+                  color:Colors.orange,
+              ),
+            );
+           } else if(user.hasData){ 
+             return const ScreenLayout();  
+             //return const SellScreen();  
+           }else{
+              return const SignInScreen();
+           }
+        }  
+       ),
+       ),
+    );
+  
+  }
+}
